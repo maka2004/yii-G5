9cb4afab8c1868450a84a070a8040322087ea482
Maksym Samoilowych <maka2004@rambler.ru>
2022-09-01 14:55:22 +0300

    Update LuckyNumbersController.php

--- controllers/LuckyNumbersController.php
+++ controllers/LuckyNumbersController.php
@@ -2,57 +2,13 @@
 
 namespace app\controllers;
 
-use app\models\LuckyNumber;
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\filters\VerbFilter;
+use app\models\LuckyNumber;
 
 class LuckyNumbersController extends Controller
 {
     const NUMBER_UNITS = 6;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['index'],
-                'rules' => [
-                    [
-                        'actions' => ['index'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'index' => ['get', 'post'],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
 
     /**
      * Displays LuckyNumbers homepage.
