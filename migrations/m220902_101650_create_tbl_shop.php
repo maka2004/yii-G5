d2c6ccd12f40cf6336c0aa7bf2c2031250877a6e
maka2004 <maka2004@rambler.ru>
2022-09-02 18:15:47 +0300

    migration Shop field size

--- migrations/m220902_101650_create_tbl_shop.php
+++ migrations/m220902_101650_create_tbl_shop.php
@@ -21,7 +21,7 @@ class m220902_101650_create_tbl_shop extends Migration
             'id' => 'INT(11) PRIMARY KEY AUTO_INCREMENT',
             'name' => $this->string(20),
             'address' => $this->string(150),
-            'area' => $this->tinyInteger(1),
+            'area' => $this->integer(1),
             'phone' => $this->string(10)
         ]);
     }
